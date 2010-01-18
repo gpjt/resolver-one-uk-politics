8aba7e8da8ee9a63dc23788ec9effbfd4088ae40
Giles Thomas <giles@giles.net>
2010-01-18 20:39:45 +0000

    A spreadsheet to load all MPs from TheyWorkForYou

---
+++ TWFYUtils.py
@@ -0,0 +1,138 @@
+from System.IO import StreamReader
+from System.Net import WebRequest
+from System.Xml import XmlNodeType, XmlTextReader
+
+
+from os import path
+
+apiKeyFile = open(path.join(path.dirname(__file__), "apikey.txt"), "r")
+apiKey = apiKeyFile.readline()
+apiKeyFile.close()
+
+twfyURL = "http://www.theyworkforyou.com/api/%s?key=%s&output=xml" % ("%s", apiKey)
+
+class TWFYFunction(object):
+    def __init__(self, functionName):
+        self.url = twfyURL % functionName
+        
+    def getData(self):
+        print self.url
+        request = WebRequest.Create(self.url)
+        response = request.GetResponse()
+        try:
+            reader = XmlTextReader(StreamReader(response.GetResponseStream()))
+            while reader.Read():
+                if reader.NodeType == XmlNodeType.Element:
+                    self.start(reader.Name)
+                elif reader.NodeType == XmlNodeType.Text:
+                    self.text(reader.Value)
+                elif reader.NodeType == XmlNodeType.EndElement:
+                    self.end(reader.Name)            
+        finally:
+            response.Close()
+            
+        return self
+
+
+class GetConstituencies(TWFYFunction):
+    
+    def __init__(self):
+        TWFYFunction.__init__(self, "getConstituencies")
+        self.inConstituency = False
+        self.constituencies = []
+                
+    def start(self, tagName):
+        if tagName == "name":
+            self.inConstituency = True
+            
+    def end(self, tagName):
+        if tagName == "name":
+            self.inConstituency = False
+            
+    def text(self, text):
+        if self.inConstituency:
+            self.constituencies.append(text)
+                
+
+class GetGeometry(TWFYFunction):
+    
+    def __init__(self, constituency):
+        TWFYFunction.__init__(self, "getGeometry")
+        self.constituency = constituency
+        self.url += "&name=" + HttpUtility.UrlEncode(constituency)
+        self.currentTag = None
+        self.fields = ("centre_lat", "centre_lon", "area", "min_lat", "max_lat", "min_lon", "max_lon", "parts")        
+                
+    def start(self, tagName):
+        if tagName in self.fields:
+            self.currentTag = tagName
+            
+    def end(self, tagName):
+        self.currentTag = None
+            
+    def text(self, text):
+        if self.currentTag is not None:
+            setattr(self, self.currentTag, text)
+            
+    def __str__(self):
+        return "Constituency %s: [%s]" % (self.constituency, [("%s=%s" % (field, getattr(self, field, None))) for field in self.fields])
+
+
+class GetMP(TWFYFunction):
+    
+    def __init__(self, constituency):
+        TWFYFunction.__init__(self, "getMP")
+        self.constituency = constituency
+        self.url += "&constituency=" + HttpUtility.UrlEncode(constituency)
+        self.currentTag = None
+        self.fields = (
+            "member_id", "house", "first_name", "last_name", "constituency", "party", 
+            "person_id", "title", "lastupdate", "full_name", "image", 
+            "entered_house", "left_house", "entered_reason", "left_reason", 
+        )        
+                
+    def start(self, tagName):
+        if tagName in self.fields:
+            self.currentTag = tagName
+            
+    def end(self, tagName):
+        self.currentTag = None
+            
+    def text(self, text):
+        if self.currentTag is not None:
+            setattr(self, self.currentTag, text)
+            
+    def __str__(self):
+        return "MP for %s: [%s]" % (self.constituency, [("%s=%s" % (field, getattr(self, field, None))) for field in self.fields])
+
+
+class MP(object):
+    pass
+
+
+class GetMPs(TWFYFunction):
+
+    def __init__(self):
+        TWFYFunction.__init__(self, "getMPs")
+        self.inMatch = False
+        self.mps = []
+        self.currentField = None
+        self.currentMP = None
+                
+    def start(self, tagName):
+        if tagName == "match":
+            self.currentMP = MP()
+            self.mps.append(self.currentMP)
+        else:
+            self.currentField = tagName
+            
+    def end(self, tagName):
+        if tagName == "match":
+            self.currentMP = None
+        
+            
+    def text(self, text):
+        if self.currentField and self.currentMP:
+            setattr(self.currentMP, self.currentField, text)
+        
+                
